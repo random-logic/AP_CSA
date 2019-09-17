dd64679ead11fc0ab5a0bc0ac742d68a61f41ab0
Andrew Jowe <ajowe034@insite.4cd.edu>
2019-09-17 15:59:49 -0700

    Finish the JAVADOCS for WS03H

--- BlueJ/Proj03/BankAccount.java
+++ BlueJ/Proj03/BankAccount.java
@@ -19,20 +19,33 @@ public class BankAccount
     }
     
     /**
+     * Purpose: Get the current balance
      * @return the current balance of the bank account
      */
     public double getBalance() {
         return balance;
     }
     
+    /**
+     * Purpose: Add money to the balance
+     * @param amount the amount to deposit to balance
+     */
     public void deposit(double amount) {
         balance += amount;
     }
     
+    /**
+     * Purpose: Subtract money from the balance
+     * @param amount the amount to withdraw from balance
+     */
     public void withdraw(double amount) {
         balance -= amount;
     }
     
+    /**
+     * Purpose: Add the amount of interest to the balance
+     * @param rate the rate of interest given to the bank account
+     */
     public void addInterest(double rate) {
         balance *= 1 + rate;
     }
