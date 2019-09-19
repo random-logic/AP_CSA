18d2028540946bf8c85817298249fc7ba5649ebc
Andrew Jowe <ajowe034@insite.4cd.edu>
2019-09-19 15:35:57 -0700

    Finish Circle class part of WS03I

--- BlueJ/Proj03/Circle.java
+++ BlueJ/Proj03/Circle.java
@@ -1,8 +1,8 @@
 /**
- * Write a description of class Circle here.
+ * Purpose: To add methods and functionality to circles such as finding the area
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Andrew
+ * @version 2019
  */
 
 import java.lang.Math;
@@ -11,15 +11,27 @@ public class Circle
 {
     double radius;
     
+    /**
+     * Construct a circle with a given radius
+     * @param r The radius of the circle
+     */
     Circle (double r) {
         radius = r;
     }
     
+    /**
+     * Get the area of the circle
+     * @return Area of the circle
+     */
     public double getArea() {
         return Math.PI * radius * radius;
     }
     
-    public double getCircumference() {
+    /**
+     * Get the circumference of the circle
+     * @return Circumference of the circle
+     */
+    public double getCircum() {
         return Math.PI * radius * 2;
     }
 }
--- BlueJ/Proj03/CircleTester.java
+++ BlueJ/Proj03/CircleTester.java
@@ -1,8 +1,8 @@
 /**
- * Write a description of class CircleTester here.
+ * Test Circle class to make sure it functions properly
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Andrew
+ * @version 2019
  */
 public class CircleTester
 {
@@ -10,10 +10,14 @@ public class CircleTester
         Circle circle1 = new Circle(1);
         Circle circle2 = new Circle(2);
         
-        System.out.println("Expected area circle 1 (Approximately): 12.5663706");
+        System.out.println("Expected area circle 1 (Approximately): 3.14159265359");
         System.out.println("Observed area circle 1: " + circle1.getArea());
         System.out.println("Expected area circle 2 (Approximately): 12.5663706");
         System.out.println("Observed area circle 2: " + circle2.getArea());
         
+        System.out.println("Expected area circle 1 (Approximately): 6.28318530718");
+        System.out.println("Observed area circle 1: " + circle1.getCircum());
+        System.out.println("Expected area circle 2 (Approximately): 12.5663706");
+        System.out.println("Observed area circle 2: " + circle2.getCircum());
     }
 }
