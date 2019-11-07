5b26ad2f1a67ec00f66edc348b4f9b5434d3b9fc
random-logic <random-logic@users.noreply.github.com>
2019-11-06 18:10:27 -0800

    WS08D (NOT COMPLETE)

--- BlueJ/WS08D/UsingLoops.java
+++ BlueJ/WS08D/UsingLoops.java
@@ -5,5 +5,162 @@
  */
 
 public class UsingLoops {
-  
+  /**
+   * Purpose: To get the future year of when the population reaches the given year
+   * @param year The initial year
+   * @param init_pop The initial population
+   * @param future_pop The target population
+   * @param rate The rate (%) determining how much the population increases per year
+   * @return
+   */
+  public static int popGrowth (int year, double init_pop, double future_pop, double rate) {
+    while (init_pop < future_pop) {
+      year++;
+      init_pop *= 1 + rate;
+    }
+
+    return year;
+  }
+
+  /**
+   * Purpose: Gets the sum of numbers from 3 to a given number inclusive
+   * @param n The given number
+   * @return The sum
+   */
+  public static int addMultOf3 (int n) {
+    int sum = 0;
+    for (int i = 3; i <= n; i += 3)
+      sum += i;
+    
+    return sum;
+  }
+
+  /**
+   * Purpose: The number of chairs in a row in a theater is k more than the number of chairs in the
+   * row before it. Given the number of chairs in certain row, calculate the number of chairs in another
+   * row (before or after that row). 
+   * @param rowNum1 row number, with count1 chairs 
+   * @param count1 number of chairs in rowNum1
+   * @param k difference in number of chairs between 2 consecutive rows
+   * @param rowNum2 row number where the number of chairs is to be calculated 
+   * @return number of chairs in rowNum2
+   */
+  public static int calcChairs (int rowNum1, int count1, int k, int rowNum2) {
+    if (rowNum1 < rowNum2)
+      for (; rowNum1 < rowNum2; rowNum1++)
+        count1 += k;
+    else
+      for (; rowNum1 > rowNum2; rowNum1--)
+        count1 -= k;
+    
+    return count1;
+  }
+
+  public static double knowledgeGrowth (double rate, double target) {
+    double months = 0;
+    double knowledge = 0;
+    
+    for (; knowledge < target; knowledge += (1 - knowledge) * rate)
+      months++;
+    
+      return months;
+  }
+
+  /**
+   * Purpose: Estimate the value of pi, based on the Gregory Series
+   * @param n the number of terms to estimate pi 
+   * @return estimated pi value
+   */
+  public static double estPi (int n) {
+    double pi_over_4 = 1;
+
+    for (int i = 2, bottom = 3; i <= n; i += 2, bottom += 4)
+      pi_over_4 -= 1 / bottom;
+
+    for (int i = 3, bottom = 5; i <= n; i += 2, bottom += 4)
+      pi_over_4 += 1 / bottom;
+
+    return 4 * pi_over_4;
+  }
+
+  /**
+   * Purpose: Display, in the Terminal/Console Window, a triangle similar to the following, where the
+   * first line has one “X” and the last has (2n-1) “X”s
+   * @param n Number of lines
+   */
+  public static void starTriangle (int n) {
+    System.out.println("Star Triangle:");
+
+    int x = 1;
+
+    for (; n > 0; n--) {
+      for (int i = 0; i < n - 1; i++)
+        System.out.print(" ");
+
+      for (int i = 0; i < x; i++)
+        System.out.print("X");
+
+      System.out.print("\n");
+
+      x += 2;
+    }
+  }
+
+  /**
+   * Purpose: Display, in the Terminal/Console Window, an n n  identity matrix, where the diagonal
+   * is 1s and the rest is 0s
+   * @param n The number of rows and columns
+   */
+  public static void identMatrix (int n) {
+    System.out.println("Identity matrix:");
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (i == j)
+          System.out.print("1");
+        else
+          System.out.print("0");
+        System.out.print(" ");
+      }
+
+      System.out.print("\n");
+    }
+  }
+
+  /**
+   * Purpose: Test the class methods
+   */
+  public static void main () {
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println();
+    System.out.println("estPi n = 5");
+    System.out.println("Expected: 3.33968254");
+    System.out.println("Observed: " + estPi(5));
+    System.out.println();
+    System.out.println("starTriangle n = 4");
+    System.out.println("Expected:");
+    System.out.println("   *");
+    System.out.println("  ***");
+    System.out.println(" *****");
+    System.out.println("*******");
+    System.out.println("Observed:");
+    starTriangle(4);
+    System.out.println();
+    System.out.println("idenMatrix n = 3");
+    System.out.println("Expected:");
+    System.out.println("1 0 0");
+    System.out.println("0 1 0");
+    System.out.println("0 0 1");
+    System.out.println("Observed:");
+    identMatrix(3);
+  }
 }
