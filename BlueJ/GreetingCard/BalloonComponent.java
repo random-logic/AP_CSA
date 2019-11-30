69007f99281e22790bb1e1ec9633be3c771dd8cd
random-logic <random-logic@users.noreply.github.com>
2019-11-29 18:36:51 -0800

    Greeting Card (Not Complete)

--- BlueJ/GreetingCard/BalloonComponent.java
+++ BlueJ/GreetingCard/BalloonComponent.java
@@ -1,35 +1,46 @@
+/**
+ * Purpose: This class displays a number of balloons.
+ * @author Andrew
+ * @version 2019
+ */
+
 import javax.swing.JComponent;
+
+import javafx.stage.Window;
+
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Color;
 import java.awt.Rectangle;
 import java.util.Random;
 
-/**
-   This class displays a number of balloons.
-*/
-public class BalloonComponent extends JComponent
+ public class BalloonComponent extends JComponent
 {
    private int balloonCount;
+   private static final int RANDOM_COLORS_SIZE = 4;
+   private static final Color[] RANDOM_COLORS = {new Color(153, 98, 54), new Color(237, 115, 46), new Color(157, 34, 30), new Color(133, 157, 60)};
    
    /**
-      Constructor for a BalloonComponent with a number of balloons
-      @param n the number of balloons to show
-   */
+    * Constructor for a BalloonComponent with a number of balloons
+    * @param n the number of balloons to show
+    */
    public BalloonComponent(int n)
    {
-       balloonCount = n;
+      balloonCount = n;
    }
-       
+      
+   /**
+    * Purpose: 
+    * @param g The graphics component
+    */
    public void paintComponent(Graphics g)
    {
-      final Color SKY_BLUE = new Color(165,218,239);
-      final int MAX_RADIUS = 30;
+      final Color SKY_ORANGE_YELLOW = new Color(248, 177, 44);
 
       Graphics2D g2 = (Graphics2D) g;
       // Draw the sky
       Rectangle sky = new Rectangle (0, 0 , getWidth(), getHeight());
-      g2.setColor(SKY_BLUE); 
+      g2.setColor(SKY_ORANGE_YELLOW); 
       g2.fill(sky);
       // Use this random number generator
       Random generator = new Random(42);
@@ -38,8 +49,19 @@ public class BalloonComponent extends JComponent
       // Your work here
       for (int i = 0; i < balloonCount; i++) {
          Balloon balloon = new Balloon(generator.nextInt(getWidth()), generator.nextInt(getHeight()), generator.nextInt(50), 
-            new Color (generator.nextFloat(), generator.nextFloat(), generator.nextFloat()));
+            randomColor());
          balloon.draw(g2);
       }
-    }
+
+      g2.drawString("Happy Thanksgiving!", Frame.getBounds().width / 2, Frame.getBounds().height / 2);
+   }
+
+   /**
+    * Purpose: Generate a random color for the balloons
+    * @return A random color
+    */
+   private static Color randomColor() {
+      Random generator = new Random();
+      return RANDOM_COLORS[generator.nextInt(RANDOM_COLORS_SIZE)];
+   }
 }
