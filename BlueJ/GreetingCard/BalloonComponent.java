80c5f2ef2d87958d9a6cc58bfabcfb4d9605daeb
random-logic <random-logic@users.noreply.github.com>
2019-11-30 19:19:19 -0800

    Finish GreetingCard

--- BlueJ/GreetingCard/BalloonComponent.java
+++ BlueJ/GreetingCard/BalloonComponent.java
@@ -8,17 +8,18 @@ import javax.swing.JComponent;
 
 import javafx.stage.Window;
 
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Color;
-import java.awt.Rectangle;
-import java.util.Random;
+import java.awt.*;
+import java.util.*;
 
  public class BalloonComponent extends JComponent
 {
    private int balloonCount;
    private static final int RANDOM_COLORS_SIZE = 4;
    private static final Color[] RANDOM_COLORS = {new Color(153, 98, 54), new Color(237, 115, 46), new Color(157, 34, 30), new Color(133, 157, 60)};
+   private static final Color SKY_ORANGE_YELLOW = new Color(248, 177, 44);
+   private static final String GREETING_MSG = "Happy Thanksgiving!";
+   private static final int GREETING_FONT_SIZE = 32;
+   private static final Color GREETING_MSG_COLOR = new Color(0, 0, 0);
    
    /**
     * Constructor for a BalloonComponent with a number of balloons
@@ -30,17 +31,15 @@ import java.util.Random;
    }
       
    /**
-    * Purpose: 
+    * Purpose: Paint the whole greeting card
     * @param g The graphics component
     */
    public void paintComponent(Graphics g)
    {
-      final Color SKY_ORANGE_YELLOW = new Color(248, 177, 44);
-
       Graphics2D g2 = (Graphics2D) g;
       // Draw the sky
       Rectangle sky = new Rectangle (0, 0 , getWidth(), getHeight());
-      g2.setColor(SKY_ORANGE_YELLOW); 
+      g2.setColor(SKY_ORANGE_YELLOW);
       g2.fill(sky);
       // Use this random number generator
       Random generator = new Random(42);
@@ -48,12 +47,16 @@ import java.util.Random;
       // Generate and draw balloons
       // Your work here
       for (int i = 0; i < balloonCount; i++) {
-         Balloon balloon = new Balloon(generator.nextInt(getWidth()), generator.nextInt(getHeight()), generator.nextInt(50), 
-            randomColor());
+         Balloon balloon = new Balloon(generator.nextInt(getWidth()), generator.nextInt(getHeight()), generator.nextInt(50), randomColor());
          balloon.draw(g2);
       }
 
-      g2.drawString("Happy Thanksgiving!", Frame.getBounds().width / 2, Frame.getBounds().height / 2);
+      Font font = new Font("TimesRoman", Font.PLAIN, GREETING_FONT_SIZE);
+      FontMetrics metrics = g.getFontMetrics(font);
+
+      g2.setFont(font);
+      g2.setColor(GREETING_MSG_COLOR);
+      g2.drawString(GREETING_MSG, (getWidth() - metrics.stringWidth(GREETING_MSG)) / 2, (getHeight() - metrics.getHeight()) / 2);
    }
 
    /**
--- BlueJ/GreetingCard/GreetingCard.java
+++ BlueJ/GreetingCard/GreetingCard.java
@@ -4,22 +4,35 @@
  * @version 2019
  */
 import javax.swing.*;
+import java.awt.*;
 
 public class GreetingCard {
+  private static final int NBALLOONS = 40;
+  
   /**
    * Purpose: Start and run the program from here
    */
   public static void main() {
     JFrame frame = new JFrame();
-    final int NBALLOONS = 40;
     
-    frame.setSize(400, 400);
+    ImageIcon icon = new ImageIcon("turkey.png");
+    Image image = icon.getImage(); 
+    image = image.getScaledInstance(120, 120, image.SCALE_SMOOTH);  
+    icon = new ImageIcon(image);
+    
+    JLabel label = new JLabel(icon);
+    label.setLocation(150, 300);
+    label.setSize(100, 100);
+    frame.add(label);
+    
+    frame.setSize(400, 600);
     frame.setTitle("BalloonViewer");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     
     BalloonComponent component = new BalloonComponent(NBALLOONS);
     frame.add(component);
     
+    frame.setResizable(false);
     frame.setVisible(true);
   }
 }
