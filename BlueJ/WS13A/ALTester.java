c9d9e9ec5c6b533ae8a5b08ecaefb9ad4c6e6d1b
Andrew Jowe <andrewjowe@yahoo.com>
2020-01-07 14:00:58 -0800

    WS13A NOT COMPLETE

---
+++ BlueJ/WS13A/ALTester.java
@@ -0,0 +1,48 @@
+/**
+ * Create an empty list.
+ *
+ * @author Andrew
+ * @version 2020
+ */
+
+import java.util.ArrayList;
+
+public class ALTester
+{
+    /**
+     * Have main function of program.
+     */
+    public static void main () {
+        ArrayList<String> cityList = new ArrayList<String>();
+        System.out.println(cityList.isEmpty()); //A
+        
+        cityList.add("San Ramon"); //B1
+        System.out.println(cityList.isEmpty());
+        
+        System.out.println(cityList.size()); //B2
+        
+        cityList.add("Dublin"); //B3
+        cityList.add("Danville");
+        System.out.println(cityList.isEmpty());
+        System.out.println(cityList.size());
+        
+        System.out.println(cityList); //C
+        System.out.println(cityList.toString());
+        
+        cityList.get(-1); //D
+        cityList.get(cityList.size());
+        cityList.get(cityList.size() + 1);
+        
+        cityList.set(0, "Pleasanton"); //E1
+        System.out.println(cityList);
+        System.out.println(cityList.size());
+        
+        cityList.remove(0); //E2
+        System.out.println(cityList);
+        System.out.println(cityList.size());
+        
+        cityList.trimToSize(); //E3
+        System.out.println(cityList);
+        System.out.println(cityList.size());
+    }
+}
---
+++ BlueJ/WS13A/G.java
@@ -0,0 +1,13 @@
+/**
+ * Write a description of class G here.
+ *
+ * @author Andrew
+ * @version 2020
+ */
+
+public class G
+{
+    public static void main () {
+        
+    }
+}
